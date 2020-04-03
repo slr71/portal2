e26388fb893e95a6f99e3541be7375a901799578
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-04-03 16:54:32 -0700

    Add user details

--- src/pages/users.js
+++ src/pages/users.js
@@ -2,31 +2,67 @@ import Layout from '../components/Layout';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
-const Index = props => (
+const Users = props => (
   <Layout>
     <h1>Users</h1>
-    <ul>
-      {props.users.map(user => (
-        <li key={user.id}>
-          <Link href="/users/[id]" as={`/users/${user.id}`}>
-            <a>{user.username}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <input type="search" placeholder="Search for ..." value={props.keyword} />
+    </div>
+    <div>
+      <table>
+        <thead>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+          <th>Institution</th>
+          <th>Occupation</th>
+          <th>Region</th>
+          <th>Country</th>
+        </thead>
+        <tbody>
+          {props.users.map(user => (
+            <tr key={user.id}>
+              <td>
+                {user.first_name}
+              </td>
+              <td>
+                {user.last_name}
+              </td>
+              <td>
+                <Link href="/users/[id]" as={`/users/${user.id}`}>
+                  <a>{user.username}</a>
+                </Link>
+              </td>
+              <td>
+                {user.institution}
+              </td>
+              <td>
+                {user.occupation.name}
+              </td>
+              <td>
+                {user.region.name}
+              </td>
+              <td>
+                {user.region.country.name}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   </Layout>
 );
 
-Index.getInitialProps = async function() {
+Users.getInitialProps = async function() {
   const res = await fetch('http://localhost:3022/users');
   const data = await res.json();
 
   console.log(`Users fetched. Count: ${data.length}`);
-  console.log(data);
 
   return {
-    users: data
+    users: data,
+    keyword: ""
   };
 };
 
-export default Index;
+export default Users;
