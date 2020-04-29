ec7acfe4e9b3ce2a45ff8d0014897413d73346a3
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-04-29 16:59:55 -0700

    Refactor SideBar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Link from "next/link";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -13,103 +14,87 @@ import HelpIcon from '@material-ui/icons/Help';
 
 const useStyles = makeStyles((theme) => ({
   nested: {
-    paddingLeft: theme.spacing(4),
+    paddingLeft: theme.spacing(4)
   }
-}));
+}))
 
-export default function SideBar() {
-  const classes = useStyles();
+const navButtons = [
+  {
+    label: "Services",
+    icon: <DashboardIcon />,
+    items: [
+      { label: "Manage Service Quotas" },
+      { label: "Tool Updates" },
+      { label: "Maintenance Calendar" }
+    ]
+  },
+  {
+    label: "Learning",
+    icon: <MenuBookIcon />,
+    items: [
+      { label: "CyVerse Learning" },
+      { label: "Getting Started" },
+      { label: "Tutorials" },
+      { label: "Focus Forum Webinars" }
+    ]
+  },
+  {
+    label: "Requests",
+    icon: <ListIcon />,
+    items: [
+      { label: "Resource Increase" },
+      { label: "Data Store Allocation Increase" },
+      { label: "Community Released Data Folders" },
+      { label: "ALL REQUESTS" }
+    ]
+  },
+  {
+    label: "Workshops",
+    icon: <EventIcon />,
+    items: [
+      { label: "View Workshops" },
+      { label: "Create/Manage Workshop" }
+    ]
+  },
+  {
+    label: "Resources",
+    icon: <HelpIcon />,
+    items: [
+      { label: "Policies" },
+      { label: "Wiki" },
+      { label: "FAQ" }
+    ]
+  }
+]
+
+const NavButton = props => {
+  const classes = useStyles()
 
   return (
     <div>
-        <ListItem button>
-          <ListItemIcon>
-            <DashboardIcon />
-          </ListItemIcon>
-          <ListItemText primary="Services" />
-        </ListItem>
-        <List component="div">
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Manage Service Quotas" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Tool Updates" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Maintenance Calendar" />
-          </ListItem>
-        </List>
-        <ListItem button>
+      <ListItem button>
         <ListItemIcon>
-            <MenuBookIcon />
+          {props.icon}
         </ListItemIcon>
-        <ListItemText primary="Learning" />
-        </ListItem>
-        <List component="div">
+        <ListItemText primary={props.label} />
+      </ListItem>
+      <List component="div">
+        {props.items.map(item => (
           <ListItem button className={classes.nested}>
-            <ListItemText primary="CyVerse Learning" />
+            <ListItemText primary={item.label} />
           </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Getting Started" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Tutorials" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Focus Forum Webinars" />
-          </ListItem>
-        </List>
-        <ListItem button>
-          <ListItemIcon>
-            <ListIcon />
-          </ListItemIcon>
-          <ListItemText primary="Requests" />
-        </ListItem>
-        <List component="div">
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Resource Increase" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Data Store Allocation Increase" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Community Released Data Folders" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="ALL REQUESTS" />
-          </ListItem>
-        </List>
-        <ListItem button>
-          <ListItemIcon>
-            <EventIcon />
-          </ListItemIcon>
-          <ListItemText primary="Workshops" />
-        </ListItem>
-        <List component="div">
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="View Workshops" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Create/Manage Workshop" />
-          </ListItem>
-        </List>
-        <ListItem button>
-          <ListItemIcon>
-            <HelpIcon />
-          </ListItemIcon>
-          <ListItemText primary="Resources" />
-        </ListItem>
-        <List component="div">
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Policies" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Wiki" />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="FAQ" />
-          </ListItem>
-        </List>
+        ))}
+      </List>
     </div>
-  );
+  )
+}
+
+export default function SideBar() {
+  return (
+    <div>
+      {navButtons.map(b =>
+        <NavButton {...b} />
+      )}
+    </div>
+  )
 }
