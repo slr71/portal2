6fc105c21e919f066291a9a1ff80d3522ecf7c25
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-05-11 15:22:55 -0700

    Add top-level components module

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Dashboard from './Dashboard'
+import DateRange from './DateRange'
+import Layout from './Layout'
+import SideBar from './SideBar'
+import SummaryCard from './SummaryCard'
+
+export { Dashboard, DateRange, Layout, SideBar, SummaryCard }
