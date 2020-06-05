0c5e95b5989210e651cc9dc52e1adc36322d0ca9
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-06-05 09:27:26 -0700

    Add user fetch to learning/resources stubs

--- src/pages/learning.js
+++ src/pages/learning.js
@@ -1,9 +1,18 @@
 import Layout from '../components/Layout'
+import { apiBaseUrl } from '../config.json'
 
 const Learning = props => (
-  <Layout>
+  <Layout {...props}>
     <h1>Learning</h1>
   </Layout>
 )
+
+Learning.getInitialProps = async function(context) {
+  //FIXME move user request into Express middleware
+  let res = await fetch(apiBaseUrl + `/users/mine`)
+  const user = await res.json()
+
+  return { user }
+}
 
 export default Learning
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,9 +1,18 @@
 import Layout from '../components/Layout'
+import { apiBaseUrl } from '../config.json'
 
 const Resources = props => (
-  <Layout>
+  <Layout {...props}>
     <h1>Resources</h1>
   </Layout>
 )
+
+Resources.getInitialProps = async function(context) {
+  //FIXME move user request into Express middleware
+  let res = await fetch(apiBaseUrl + `/users/mine`)
+  const user = await res.json()
+
+  return { user }
+}
 
 export default Resources
