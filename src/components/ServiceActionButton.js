cc40ea838f1f68153d1bb396304466de4e083681
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-07-20 12:41:11 -0700

    Add button for service actions

---
+++ src/components/ServiceActionButton.js
@@ -0,0 +1,31 @@
+import { Button } from '@material-ui/core'
+
+
+const ServiceActionButton = props => {
+  const { user, service, requestAccessHandler } = props
+  let label, action, url
+
+  // Request status can be: 'granted', 'denied', 'approved', 'pending'
+  if (service.is_powered)
+    [label, action, url] = ['LAUNCH', null, service.service_url]
+  else {
+    const [userService] = user.services.filter(s => s.id == service.id)
+
+    if (!userService || !userService.request)
+      [label, action, url] =  ['REQUEST ACCESS', requestAccessHandler, null]
+    else if (userService.request.status === 'granted')
+      [label, action, url] =  ['LAUNCH', null, service.service_url]
+    else if (userService.request.status === 'denied')
+      [label, action, url] =  ['DENIED']
+    else
+      [label, action, url] =  ['PENDING']
+  }
+
+  return (
+    <Button variant="contained" color="primary" size="medium" href={url} onClick={action}>
+      {label}
+    </Button> 
+  )
+}
+
+export default ServiceActionButton
