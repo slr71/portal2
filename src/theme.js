e29a2dc8923b3f1048fc696cb07f18be49a4d3ab
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-07-29 14:53:36 -0700

    Change sidebar menu selection color to match header

--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,39 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles'
+import { red } from '@material-ui/core/colors'
 
-// Create a theme instance.
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#0971ab',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
+const palette = {
+  primary: {
+    main: '#0971ab',
   },
-});
+  secondary: {
+    main: '#19857b',
+  },
+  error: {
+    main: red.A400,
+  },
+  background: {
+    default: '#fff',
+  },
+}
+
+const theme = createMuiTheme({
+  palette: palette,
+  overrides: {
+    MuiListItem: {
+      root: {
+        "&$selected": {
+          backgroundColor: palette.primary.main,
+          color: "white",
+          "&:hover": {
+            backgroundColor: palette.primary.main,
+          },
+          "& .MuiSvgIcon-root": {
+            fill: "white"
+          }
+        }
+      }
+    }
+  }
+})
 
 export default theme;
