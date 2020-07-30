9054f4d7ae9141b4a5d30221b44ac468ea3a18e9
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-07-30 15:28:45 -0700

    Fix syntax

--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,15 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import { red } from '@material-ui/core/colors'
 
-// Create a theme instance.
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#0971ab',
-    },
-    secondary: {
-      main: '#89e2fa',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
+const palette = {
+  primary: {
+    main: '#0971ab',
+  },
+  secondary: {
+    main: '#89e2fa',
+  },
+  error: {
+    main: red.A400,
   },
   background: {
     default: '#fff',
