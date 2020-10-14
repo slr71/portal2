d82864d69a5cdcbebdfe2d28a954015e744a925d
mgwall17 <mgwall17@gmail.com>
2020-10-14 02:32:29 -0700

    added custom 404 page

---
+++ src/components/ErrorAnimation.js
@@ -0,0 +1,34 @@
+import React, {useEffect, createRef} from "react"
+import lottie from 'lottie-web'
+import { makeStyles } from '@material-ui/core'
+import animation from '../animations/erroranimation.json'
+
+const useStyles = makeStyles((theme) => ({
+  animationBox: {
+    maxWidth:'560px',
+    margin: '0 auto',
+    marginTop:'10px',
+  },
+}))
+
+const Animation = () => {
+  let animationContainer = createRef();
+  
+  const classes = useStyles()
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: animationContainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animation
+    });
+  }, []);
+
+  return (
+    <div className={classes.animationBox} ref={animationContainer} />
+  );
+};
+
+export default Animation;
---
+++ src/pages/404.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import ErrorAnimation from '../components/ErrorAnimation'
+import { Grid, Box, Typography } from '@material-ui/core'
+
+export default function Custom404() {
+    return (
+        <div>
+            <Box>
+                <ErrorAnimation />
+                <Grid
+                    container
+                    alignItems="center"
+                    direction="row"
+                    justify="center"
+                    wrap="nowrap"
+                >
+                    <Typography variant="h4" style={{ marginTop: '3%' }}>
+                        {' '}
+                        <strong> Whoops!</strong> Page Not Found{' '}
+                    </Typography>
+                </Grid>
+                <Grid
+                    container
+                    alignItems="center"
+                    direction="row"
+                    justify="center"
+                    wrap="nowrap"
+                >
+                    <Typography variant="h6" style={{ marginTop: '10px' }}>
+                        {' '}
+                        Looks like we got a little lost in space.
+                    </Typography>
+                </Grid>
+                <Grid
+                    container
+                    alignItems="center"
+                    direction="row"
+                    justify="center"
+                    wrap="nowrap"
+                >
+                    <Typography variant="h6" style={{ marginTop: '10px' }}>
+                        {' '}
+                        Let's get you back to{' '}
+                        <a
+                            href="https://www.cyverse.org"
+                            style={{ textDecoration: 'none' }}
+                        >
+                            CyVerse
+                        </a>
+                        .
+                    </Typography>
+                </Grid>
+            </Box>
+        </div>
+    )
+}
