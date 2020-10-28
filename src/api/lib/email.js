7967dbbc294f45e4ef73242258299793196461bf
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-10-28 16:51:20 -0700

    Add support for html email templates

--- src/api/lib/email.js
+++ src/api/lib/email.js
@@ -7,26 +7,41 @@ const { hmacKey } = require('../../config');
 const ALGO = 'aes-256-cbc';
 
 function renderEmail({ to, bcc, subject, templateName, fields }) {
-    // Load and populate template
-    const templatePath = path.join(__dirname, '..', 'templates', templateName + '.txt'); //TODO support htlm templates
-    let body = fs.readFileSync(templatePath, 'utf8').toString(); 
-    for (f in fields) {
-        const regex = new RegExp('\\$\\{' + f + '\\}', 'gi');
-        body = body.replace(regex, fields[f]);
+    let body = {};
+
+    // Load and populate email template
+    for (let ext of ['html', 'txt']) {
+        const templatePath = path.join(__dirname, '..', 'templates', `${templateName}.${ext}`);
+        if (fs.existsSync(templatePath)) {
+            body[ext] = fs.readFileSync(templatePath, 'utf8').toString(); 
+            for (f in fields) {
+                const regex = new RegExp('\\$\\{' + f + '\\}', 'gi');
+                body[ext] = body[ext] .replace(regex, fields[f]);
+            }
+            if (!body[ext])
+                throw('Empty email template');
+            break; // only load txt template if html template doesn't exist
+        }
     }
 
-    if (Array.isArray(bcc))
-        bcc = bcc.join(',')
-    
-    // console.log(body);
-    return sendmail({
-        from: 'support@cyverse.org', //FIXME hardcoded
+    const config = {
+        from: 'support@cyverse.org', //FIXME move into config file
         to,
-        bcc,
-        subject,
-        text: body
-        //html: message
-    });
+        subject
+    };
+
+    if (bcc) {
+        if (Array.isArray(bcc))
+            bcc = bcc.join(',');
+        config['bcc'] = bcc;
+    }
+
+    if (body['html'])
+        config.html = body['html']
+    else
+        config.text = body['txt']
+
+    return sendmail(config);
 }
 
 function key() {
