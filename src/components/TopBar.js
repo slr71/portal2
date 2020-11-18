b2dcff05708ef62c25d1b1cddf53e9b9784115cf
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-11-17 21:24:00 -0700

    Tweak padding

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
     height: "3.75em",
   },
   breadcrumbs: {
-    paddingTop:'3px',
+    paddingTop:'0.4em',
   }
 }))
 
@@ -62,7 +62,7 @@ const TopBar = (props) => {
   const backUrl = "/" + parts.slice(0, -1).join("/")
 
   return (
-    <Box pl={3} pr={4} p={1} className={classes.box}>
+    <Box px={4} pt={1.15} className={classes.box}>
       <Grid container justify="space-between">
         <Grid item>
           {props.back 
