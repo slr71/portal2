cb5d8eb476ba59012a4cf415c4bf1a4bd8b5b0cb
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-12-18 16:32:44 -0700

    Add unread message badge to Help menu item

--- src/components/CustomIntercom.js
+++ src/components/CustomIntercom.js
@@ -1,86 +1,65 @@
-/**
- *  Based on Sonora module of same name 10/2/2020
- * 
+/*
+ *  Based on Sonora module of same name
  */
 
-import { Badge, Button, IconButton, Tooltip, useTheme } from "@material-ui/core"
+import { Badge, Button, Tooltip, } from "@material-ui/core"
 import LiveHelpIcon from "@material-ui/icons/LiveHelp"
 import { useUser } from '../contexts/user'
 import config from '../config.json'
 
-function CustomIntercom(props) {
-    const theme = useTheme()
-    const [user] = useUser()
+function CustomIntercom() {
+  const [user] = useUser()
+  const [unreadCount, setUnreadCount] = React.useState(0)
 
-    React.useEffect(() => {
-        if (user) {
-            intercomLogin(
-                user.username,
-                user.email,
-                config.intercom.appId,
-                config.intercom.companyId,
-                // config.intercom.companyName
-            )
+  React.useEffect(() => {
+    if (!user || !window.Intercom) // should never happen
+      return
 
-            // return () => {
-            //     intercomLogout()
-            // }
-        }
+    window.Intercom("boot", {
+      app_id: config.intercom.appId,
+      email: user.email,
+      user_id: user.username,
+      created_at: Date.now(),
+      company: {
+          id: config.intercom.companyId,
+          //name: companyName,
+      },
     })
 
-    const handleClick = () => {
-        window.Intercom('show')
-    }
-
-    return (
-      <Tooltip title="Chat with CyVerse Support">
-        <Button
-          variant="text"
-          color="inherit"
-          size="large"
-          startIcon={<LiveHelpIcon />}
-          onClick={handleClick}
-        >
-          Help
-        </Button>
-        {/* <IconButton
-          // style={{ color: theme.palette.primary.contrastText }}
-          onClick={handleClick}
-        >
-          <Badge>
-            <LiveHelpIcon fontSize='large' style={{fill: 'white'}} />
-          </Badge>
-        </IconButton> */}
-      </Tooltip>
+    window.Intercom(
+      "onUnreadCountChange",
+      (newUnreadCount) => setUnreadCount(newUnreadCount)
     )
-}
+  })
 
-function intercomLogin(userId, email, appId, companyId, companyName) {
-  if (window.Intercom) {
-      window.Intercom("boot", {
-          app_id: appId,
-          email: email,
-          user_id: userId,
-          created_at: Date.now(),
-          company: {
-              id: companyId,
-              name: companyName,
-          },
-      })
+  const handleClick = () => {
+    window.Intercom('show')
   }
+
+  return (
+    <Tooltip title="Chat with CyVerse Support">
+      <Button
+        variant="text"
+        color="inherit"
+        size="large"
+        startIcon={<Badge badgeContent={unreadCount} color="error"><LiveHelpIcon /></Badge>}
+        onClick={handleClick}
+      >
+        Help
+      </Button>
+    </Tooltip>
+  )
 }
 
 function intercomLogout() {
   console.log("intercom logging out")
-  if (window.Intercom) {
-      window.Intercom("shutdown")
-  }
+  if (window.Intercom)
+    window.Intercom("shutdown")
 }
 
 function intercomShow() {
-  if (window.Intercom) {
-      window.Intercom("show")
-  }
+  if (window.Intercom)
+    window.Intercom("show")
 }
 
 export { CustomIntercom, intercomShow }
