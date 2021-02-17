0ef8883908e2029b11adc656a6bad4fe1dc579bd
Matt Bomhoff <mbomhoff@email.arizona.edu>
2021-02-17 09:59:48 -0700

    Fix Intercom infinite message problem

--- src/components/CustomIntercom.js
+++ src/components/CustomIntercom.js
@@ -13,25 +13,29 @@ function CustomIntercom() {
   const [unreadCount, setUnreadCount] = React.useState(0)
 
   React.useEffect(() => {
-    if (!user) {// should never happen
+    if (typeof window.Intercom === "function") {
+      window.Intercom("reattach_activator")
+      window.Intercom("update", window.intercomSettings)
+      return
+    }
+
+    if (!user) { // should never happen
       console.warn('CustomIntercom: missing user')
       return
     }
 
      // Initialize Intercom chat widget
      // UP-66: moved all initialization code due to race condition between this useEffect hook and the one in _app.js
-    if (!window.Intercom) {
-      window.intercomSettings = {
-        app_id: config.intercom.appId,
-        alignment: "right",
-        hide_default_launcher: true
-      }
-
-      // Load Intercom library -- copied from developer docs, modified app ID
-      // https://developers.intercom.com/installing-intercom/docs/basic-javascript
-      console.log('Initializing Intercom widget')
-      var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src=`https://widget.intercom.io/widget/${config.intercom.appId}`;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}
+    window.intercomSettings = {
+      app_id: config.intercom.appId,
+      alignment: "right",
+      hide_default_launcher: true
     }
+
+    // Load Intercom library -- copied from developer docs, modified app ID
+    // https://developers.intercom.com/installing-intercom/docs/basic-javascript
+    console.log('Initializing Intercom widget')
+    var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src=`https://widget.intercom.io/widget/${config.intercom.appId}`;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}
 
     window.Intercom("boot", {
       app_id: config.intercom.appId,
@@ -48,10 +52,6 @@ function CustomIntercom() {
       "onUnreadCountChange",
       (newUnreadCount) => setUnreadCount(newUnreadCount)
     )
-
-    return () => {
-      intercomLogout()
-    }
   })
 
   return (
@@ -67,12 +67,6 @@ function CustomIntercom() {
       </Button>
     </Tooltip>
   )
-}
-
-function intercomLogout() {
-  console.log("intercom logging out")
-  if (window.Intercom)
-    window.Intercom("shutdown")
 }
 
 function intercomShow() {
