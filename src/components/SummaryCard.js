d5c93c87079358c183fb303aebb46eecf3ed8694
Matt Bomhoff <mbomhoff@email.arizona.edu>
2021-03-30 17:41:36 -0700

    Fix card text trucation

--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -2,9 +2,6 @@ import { Card, CardHeader, CardContent, CardActions, Typography, Avatar } from '
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
-  content: {
-    minHeight: '6em'
-  },
   title: {
     lineHeight: '1.1',
     fontSize: '1.4em',
@@ -15,6 +12,14 @@ const useStyles = makeStyles((theme) => ({
       fontSize: '1.2em',
       fontWeight: '400'
     }
+  },
+  description: {
+    // line clamp: https://css-tricks.com/almanac/properties/l/line-clamp/
+    display: '-webkit-box',
+    '-webkit-line-clamp': 2,
+    '-webkit-box-orient': 'vertical',
+    overflow: 'hidden',
+    minHeight: '2.5em'
   }
 }))
 
@@ -35,9 +40,9 @@ const SummaryCard = ({ title, subtitle, description, iconUrl, icon, action, larg
         subheader={subtitle}
         titleTypographyProps={{className: classes.title}}
       />
-      <CardContent className={classes.content}>
-        <Typography variant="body2" color="textPrimary" component="p" className={classes.descriptionText}>
-          {description.length > 130 ? description.substring(0, 130) + ' ...' : description}
+      <CardContent>
+        <Typography variant="body2" color="textPrimary" component="p" className={classes.description}>
+          {description}
         </Typography>
       </CardContent>
       <CardActions>
