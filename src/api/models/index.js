5bdb469cc93a38a968d99e7d3f6cc410cc7703a9
Matt Bomhoff <mbomhoff@email.arizona.edu>
2021-11-01 16:23:14 -0700

    Remove unused db logging option for now

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -24,7 +24,7 @@ const sequelize = new Sequelize(
   process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
   { host: process.env.DB_HOST,
     dialect: 'postgres',
-    //logging: process.env.DB_LOGGING,
+    logging: false,//process.env.DB_LOGGING,
     define: {
       timestamps: false,
       freezeTableName: true,
