df11805443c2aa3112b9b260d1c57f04e3b2c1c1
Matt Bomhoff <mbomhoff@email.arizona.edu>
2021-11-01 16:25:38 -0700

    Fix typo

--- src/components/CustomIntercom.js
+++ src/components/CustomIntercom.js
@@ -48,7 +48,7 @@ function CustomIntercom() {
     // Load Intercom library -- copied from developer docs, modified app ID
     // https://developers.intercom.com/installing-intercom/docs/basic-javascript
     console.log('Initializing Intercom widget')
-    var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src=`https://widget.intercom.io/widget/${iconfig.INTERCOM_APP_ID}`;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}
+    var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src=`https://widget.intercom.io/widget/${config.INTERCOM_APP_ID}`;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}
 
     window.Intercom("boot", {
       app_id: config.INTERCOM_APP_ID,
